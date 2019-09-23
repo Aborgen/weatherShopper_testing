eb937152a996e8deb5602266771d8397fb2dd63c
Aborgen <alexanderborgen@gmail.com>
2019-09-23 16:14:24 -0700

    Add _verifyPage method to Page, which ensures that selenium driver is on the correct page

--- src/pages/Page.py
+++ src/pages/Page.py
@@ -12,7 +12,13 @@ class Page():
     self._baseUrl = baseUrl
     self._path = path
     self._previousUrl = previousUrl
+    # If previousUrl is None, that means the driver has yet to 'get' a page.
+    if self._previousUrl != None:
+      self._verifyPage()
 
   def load(self):
     self._driver.get(self._baseUrl + self._path)
+    self._verifyPage()
 
+  def _verifyPage(self):
+    assert self._driver.current_url.rstrip('//') == self._baseUrl + self._path
