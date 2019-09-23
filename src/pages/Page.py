a3dcbd86431083f4ea2ad1b13f096de348e5dbb6
Aborgen <alexanderborgen@gmail.com>
2019-09-23 16:15:44 -0700

    Create _pageTrail method which prints whenever a class that derives from Page is instantiated

--- src/pages/Page.py
+++ src/pages/Page.py
@@ -12,6 +12,7 @@ class Page():
     self._baseUrl = baseUrl
     self._path = path
     self._previousUrl = previousUrl
+    self._pageTrail()
     # If previousUrl is None, that means the driver has yet to 'get' a page.
     if self._previousUrl != None:
       self._verifyPage()
@@ -19,6 +20,16 @@ class Page():
   def load(self):
     self._driver.get(self._baseUrl + self._path)
     self._verifyPage()
+
+  def _pageTrail(self):
+    fullUrl = self._baseUrl + self._path
+    if self._previousUrl == None:
+      print(f"Start:\t{fullUrl}")
+    else:
+      print("-----")
+      print(f"From:\t{self._previousUrl}")
+      print(f"To:\t{fullUrl}")
+
 
   def _verifyPage(self):
     assert self._driver.current_url.rstrip('//') == self._baseUrl + self._path
