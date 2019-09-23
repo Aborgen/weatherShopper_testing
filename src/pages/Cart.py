c83102f31f41c6bd444663d69f819317c112db8a
Aborgen <alexanderborgen@gmail.com>
2019-09-23 16:12:36 -0700

    Update verifyItems and verifyTotal to cast any numbers from str to int

--- src/pages/Cart.py
+++ src/pages/Cart.py
@@ -7,27 +7,29 @@ class Cart(Page):
   def __init__(self, driver: webdriver, baseUrl: str, previousUrl: Optional[str] = None):
     super().__init__(driver, baseUrl, self.__class__.PATH(), previousUrl)
 
-  def verify(self, expectedItems: List[Item]):
+  def verify(self, expectedItems: List[Item]) -> bool:
     itemPairs = []
     total = 0
     for name, price, _ in expectedItems:
       itemPairs.append((name, price))
-      total += int(price)
+      total += price
 
     return self.verifyTotal(total) and self.verifyItems(itemPairs)
 
-  def verifyItems(self, itemPairs: List[Tuple[str, str]]):
+  def verifyItems(self, itemPairs: List[Tuple[str, int]]) -> bool:
     rows = self._driver.find_elements_by_xpath("//table/tbody/tr")
     presentPairs = []
     for row in rows:
-      item = tuple(x.text for x in row.find_elements_by_tag_name("td"))
-      presentPairs.append(item)
-    
-    return sorted(itemPairs) == sorted(presentPairs)
+      name, price = row.find_elements_by_tag_name("td")
+      name = name.text
+      price = int(price.text)
+      presentPairs.append((name, price))
 
-  def verifyTotal(self, expectedTotal: int):
-    presentTotal = self._driver.find_element_by_id('total').text.split(' ')[-1]
-    return expectedTotal == int(presentTotal)
+    return sorted(presentPairs) == sorted(itemPairs)
+
+  def verifyTotal(self, expectedTotal: int) -> bool:
+    presentTotal = int(self._driver.find_element_by_id('total').text.split(' ')[-1])
+    return presentTotal == expectedTotal
 
   @staticmethod
   def PATH() -> str:
